f2e52c1aa54026248df0d4b88396049ce6c99825
Moryu <moryu999@gmail.com>
2021-10-24 13:57:58 +0900

    style : 不要なコード削除

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include "Ambient_private_def.hpp"
 #include "EnvironmentLoggerModel.hpp"
 #include <Arduino.h>
-#include <Wire.h>
-//#include <WiFi.h>
 #include "Ambient.h"
 #include "esp_deep_sleep.h"
 #include "time.h"
@@ -11,22 +9,12 @@
 const char *ntpServer        = "ntp.jst.mfeed.ad.jp";
 const long gmtOffset_sec     = 9 * 3600;
 const int daylightOffset_sec = 0;
-static uint8_t
-conv2d(const char *p); // Forward declaration needed for IDE 1.6.x
-uint8_t hh = conv2d(__TIME__), mm = conv2d(__TIME__ + 3),
-        ss  = conv2d(__TIME__ + 6); // Get H, M, S from compile time
-byte xcolon = 0, xsecs = 0;
 
 WiFiClient client;
 Ambient ambient;
 
 MH_Z14 *p_mh_z14;
 BME280 *p_bme280;
-// BME280 bme280(HSPI, 2, 5, 36, 35);
-// C LK 青 2
-// CS 緑 35
-// MISO/SDO 紫 5
-// MOSI/SDI 灰 36
 
 String _time_str = "";
 String _co2_str  = "";
@@ -51,13 +39,6 @@ void LCDprint_environmentaldata() {
     M5.Lcd.println(_hum_str);
 }
 
-// Function to extract numbers from compile time string
-static uint8_t conv2d(const char *p) {
-    uint8_t v = 0;
-    if('0' <= *p && *p <= '9') v = *p - '0';
-    return 10 * v + *++p - '0';
-}
-
 hw_timer_t *samplingTimer = NULL;
 void IRAM_ATTR onTimer0() {
     static uint32_t pushed_flag_and_timecount = 0;
@@ -78,6 +59,7 @@ void IRAM_ATTR onTimer0() {
 void setup() {
     p_mh_z14 = get_ptr_mhz14();
     p_bme280 = get_ptr_bme280();
+
     M5.begin();
     Serial.begin(9600);
     M5.Lcd.println("Hello!");
@@ -95,10 +77,8 @@ void setup() {
     ambient.begin(AMB_CHANNEL_ID, (const char *)AMB_WRITE_KEY, &client);
 
     samplingTimer = timerBegin(0, 8000, true); // 100usのタイマーを初期設定する
-    timerAttachInterrupt(samplingTimer, &onTimer0,
-                         true); // 割り込み処理関数を設定する
-    timerAlarmWrite(samplingTimer, 100 * 10,
-                    true); // 100msのタイマー値を設定する
+    timerAttachInterrupt(samplingTimer, &onTimer0, true); // 割り込み処理関数を設定する
+    timerAlarmWrite(samplingTimer, 100 * 10,  true); // 100msのタイマー値を設定する
 
     timerAlarmEnable(samplingTimer); // タイマーを起動する
 
@@ -113,16 +93,15 @@ void loop() {
     while(WiFi.status() != WL_CONNECTED) { //  Wi-Fi AP接続待ち
         wifi_request_count++;
         if(wifi_request_count > 100) {
+            /* 100回WiFiに接続失敗したら */
             M5.Lcd.println("Wifi connection error!");
             WiFi.begin(WIFI_SSID, WIFI_KEY);
             wifi_request_count = 0;
         }
         delay(100);
     }
-    // M5.Lcd.clear();
-    // M5.Lcd.setCursor(0,0);
-    // M5.Lcd.setTextSize(2);
 
+    /* ambientに接続 */
     ambient.begin(AMB_CHANNEL_ID, (const char *)AMB_WRITE_KEY, &client);
 
     /* CO2取得 */
@@ -131,29 +110,26 @@ void loop() {
     _co2_str = "CO2   : ";
     _co2_str += String(_co2ppm);
     _co2_str += String(" [ppm]");
-    // M5.Lcd.println(_co2_str);
 
     /* 気温取得 */
     float _temp_f = (float)p_bme280->readTempDeg();
     _temp_str     = "temp  : ";
     _temp_str += String(_temp_f);
     _temp_str += String(" [deg]");
-    // M5.Lcd.println(_temp_str);
 
     /* 気圧取得 */
     float _press_f = (float)p_bme280->readPreshPa();
     _pres_str      = "press : ";
     _pres_str += String(_press_f);
     _pres_str += String(" [hPa]");
-    // M5.Lcd.println(_pres_str);
 
     /* 湿度取得 */
     float _hum_f = (float)p_bme280->readHumidPcnt();
     _hum_str     = "humd  : ";
     _hum_str += String(_hum_f);
     _hum_str += String(" [%]");
-    // M5.Lcd.println(_hum_str);
 
+    /* 気圧が不正な値になることがあるので、700hPa以上ならambientに送信 */
     if(_press_f > 700.0f) {
         ambient.set(1, String(_temp_f).c_str());
         ambient.set(2, String(_press_f).c_str());
@@ -165,6 +141,7 @@ void loop() {
 
     struct tm timeinfo;
     getLocalTime(&timeinfo);
+
     _time_str = String(timeinfo.tm_year + 1900);
     _time_str += String(" / ");
     _time_str += String(timeinfo.tm_mon + 1);
@@ -177,11 +154,12 @@ void loop() {
 
     WiFi.disconnect(true);
 
+    /* 1分毎に更新する処理 */
     static uint32_t last_process_time_ms = 0;
     while(millis() - last_process_time_ms < 60 * 1000) {
         delay(100);
         M5.update();
     }
     last_process_time_ms = millis();
-    // delay(60*1000);
+
 }
